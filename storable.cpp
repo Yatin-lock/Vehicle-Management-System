89367dc36abfd9fb02a7cc0367c6f4d255e6e526
Yatin-lock <yatinutreja678@gmail.com>
2021-10-21 15:44:05 +0530

    second commit

--- storable.cpp
+++ storable.cpp
@@ -1,9 +1,30 @@
 #ifndef storable_cpp
 #define storable_cpp
-#endif
 
 // #include "serializable.cpp"
+class Serializable{
+    
+};
 
+#include<string>
+#include<iostream>
 
+using namespace std;
 
+class Storable: public Serializable{
+protected:
+    long recordId;
+public:
+    Storable(long recordId){
+        this->recordId = recordId;
+    }
+    long getRecord() const{
+        return this->recordId;
+    }
+    virtual void setDataFrom(Storable *s) = 0;
+    template<typename T> friend class Table;
+    friend class Database;
+};
+
+#endif
 
