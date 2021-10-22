800c777d604a21269ee9348cb914b00e7e9d8757
Yatin-lock <yatinutreja678@gmail.com>
2021-10-22 19:56:49 +0530

    commit 7

---
+++ table.h
@@ -0,0 +1,28 @@
+#ifndef table_h
+#define table_h
+#include "storable.cpp"
+#include "error.cpp"
+#include<bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+class Table{
+
+    string filename;
+    fstream fileStream;
+    vector<Storable*> records = nullptr;
+
+    T *getReferenceOfRecordForId(long recordId) const throw (NoSuchRecordError);
+    void writeToFile() throw (IOError);
+    const T* const addNewRecord(T data) throw (MemoryError, IOError);
+    void updateRecord(T updatedRecord) throw (IOError, NoSuchRecordError);
+public:
+    Table(string filename) throw MemoryError;
+    ~Table();
+    long getNextRecordId() const;
+    const T *const  getRecordForId(long recordId) const throw NoSuchRecordError;
+    friend class Database;
+};
+
+#endif
